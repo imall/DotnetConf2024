97a5eb47a8bd94729caaadab46c0b466817fcdb4
imall <imall841110@gmail.com>
2024-12-08 17:12:39 +0800

    設定標題、版本、描述
    (cherry picked from commit 17413dd3075925bd68cc53cc31423d41c37ed57d)
    (cherry picked from commit af1fc3a1d44b0d38a0dc39b87972fb4407e4ceda)
    (cherry picked from commit b79d56ca70a3e10824f33185bcab9fac3895f4a2)

--- Application/SwaggerSettings.cs
+++ Application/SwaggerSettings.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace Application;
 
 public static class SwaggerSettings
@@ -8,7 +10,27 @@ public static class SwaggerSettings
     /// <param name="services"></param>
     public static void AddSwaggerSettings(this IServiceCollection services)
     {
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(setupAction =>
+        {
+            setupAction.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "Dotnet conf 2024 範例專案",
+                    Version = "v1",
+                    Description = "Dotnet conf 2024 Taiwan Swagger 範例",
+                    TermsOfService = new Uri("https://dotnetconf.study4.tw/Code-Of-Conduct"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Study4tw",
+                        Url = new Uri("https://www.facebook.com/groups/study4tw/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    },
+                });
+        });
     }
     
     /// <summary>
